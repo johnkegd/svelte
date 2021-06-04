3fbd44a63ad8df24ee50359bf29aee50d0f3b34c
John Garcia <john.garcia@ajila.com>
2021-06-04 12:43:58 +0200

    project-structure: added package dir and site to init npm packages by dir

---
+++ packages/sections/Section.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let sectionStyles = "relative py-20";
+    export let containerStyles = "container mx-auto px-4";
+
+</script>
+
+{#if containerStyles != "false"}
+    <section class="{sectionStyles}">
+        <slot name="head-section"></slot>  
+        <div class="{containerStyles}">
+            <slot name="images-section"></slot>
+            <slot></slot>
+        </div>
+        <slot name="footer-section"></slot>
+    </section>
+{:else }
+    <section class="{sectionStyles}">
+        <slot></slot>
+    </section>
+{/if}
--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+const pkg = require('./package.json');
 
-const name = pkg.name
+/* const name = pkg.name
 	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
 	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+	.replace(/-\w/g, m => m[1].toUpperCase()); */
 
 export default {
 	input: 'src/Card.svelte',
 	output: [
 		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
+		{ file: pkg.main, 'format': 'umd', name: 'Card' }
 	],
 	plugins: [
 		svelte(),
