2e7b637f9622445f029a1825e8713800c630764b
John Garcia <johnkegd@gmail.com>
2022-05-16 20:31:20 +0200

    [utils] - Sizes module detecting svelte app

--- packages/utils/threejs/Sizes.js
+++ packages/utils/threejs/Sizes.js
@@ -1,21 +1,36 @@
-import EventEmitter from './EventEmitter.js'
+import EventEmitter from "./EventEmitter.js";
 
 export default class Sizes extends EventEmitter {
-    constructor() {
-        super()
+  constructor(svelteListener) {
+    super();
 
-        // Setup
+    if (svelteListener) {
+      //test
+      this.resizeHandler = this.getResizeHandler.bind(this);
+    } else {
+      // Setup
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+      // Resize event
+      window.addEventListener("resize", () => {
         this.width = window.innerWidth;
         this.height = window.innerHeight;
         this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
-        // Resize event
-        window.addEventListener('resize', () => {
-            this.width = window.innerWidth;
-            this.height = window.innerHeight;
-            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
-
-            this.trigger('resize');
-        })
+        this.trigger("resize");
+      });
     }
+  }
+
+  getResizeHandler() {
+    var _self = this;
+    console.log("external listener ", this);
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+    this.trigger("resize");
+  }
 }
